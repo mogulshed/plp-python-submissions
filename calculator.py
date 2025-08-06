d688ae8c513ec764c4b845028af38efbc77e3261
mogulshed <seniormogulsheddy@gmail.com>
2025-08-06 11:40:07 +0300

    Week 1 basic calculator submission

---
+++ calculator.py
@@ -0,0 +1,25 @@
+#basic calculator program
+name= input("Whats your name?? :")
+print("Hello", name ,"welcome to the Basic Calculator")
+
+#asking the user for the input
+num1=float(input("Enter your first number :"))
+operator=input("Enter your Ooerator (either +,-,* or /) :")
+num2=float(input("Enter your Second number :"))
+
+#the calculator operation
+if operator == "+":
+    result= num1 +num2
+    print(f"{num1}+{num2}= {result}")
+elif operator == "-":
+    result= num1-num2
+    print(f"{num1}-{num2}= {result}")
+elif operator== "*":
+    result= num1 * num2
+    print(f"{num1}* {num2}= {result}")
+elif operator == "/":
+    if num2 != 0:
+        result= num1 / num2
+        print(f"{num1} / {num2} = {result}")
+else:
+    print("INVALID OPERATOR!!! Please input one of the operators provided:")
